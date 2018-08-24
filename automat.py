28dfcaa4578778c2ecbfd72df032f9fb14335c3b
everton-zoio <evertonabreu@alunos.utfpr.edu.br>
2018-08-23 21:04:22 -0300

    estrutura dos Estados e das Transições

--- automat.py
+++ automat.py
@@ -1,37 +1,77 @@
 import sys
 
 class Transition():
-	__state = 0
-	__newState = 0
-	__value = ''
-	__newValue = ''
-	__direction = ''
+	state = 0
+	newState = 0
+	value = ''
+	newValue = ''
+	direction = ''
 
-	def __init__(self, date):
-		data = date.split()
-		self.__state = int(data[0])
-		self.__newState = int(data[1])
-		self.__value = data[2]
-		self.__newValue = data[3]
-		self.__direction = data[4]
+	def __init__(self, trans, state, states):
+		data = trans.split()
+		self.state = state
+		self.newState = int(data[1])
+		for state in states:
+			if(self.newState == state.cod):
+				self.newState = state
+				break
+		self.value = data[2]
+		self.newValue = data[3]
+		self.direction = data[4]
 		pass
 	
+	def valid(self, value):
+		if(self.value == value):
+			return 1
+		return 0
+		pass
+
 	def print(self):
-		print(self.__state, self.__newState, self.__value, self.__newValue, self.__direction)
+		print("\t",self.state.cod, self.newState.cod, self.value, self.newValue, self.direction)
 		pass
 	pass
 
 class State():
+	def __init__(self, cod, fStates):
+		self.cod = cod
+		self.type = 0
+		for fS in fStates:
+			if(fS == self.cod):
+				self.type = 1
+				break
+		self.transitions = []
+		pass
 	
+	def mount(self, trans, states):
+		for tr in trans:
+			if(int(tr.split()[0]) == self.cod):
+				self.transitions.append(Transition(tr, self, states))
+		pass
+	def print(self):
+		print("  ", self.cod)
+		for tr in self.transitions:
+			tr.print()
+		print("\n")
+		pass
 
 class Machine():
 
 	def __init__(self):
-
+		self.states = 0
 		pass
 	pass
 
-arq = open(sys.argv[1], )
-data = arq.readlines()
-trans = Transition(data[7])
-trans.print()
+
+if(sys.argv.__len__() >= 2):
+	arq = open(sys.argv[1],)
+	data = arq.readlines()
+	dataStates = list(map(int, data[3].split()))
+	dataTrans = data[7:]
+	fStates = data[5].split()
+	fStates = list(map(int, fStates))
+	states = []
+	for st in dataStates:
+		states.append(State(st,fStates))
+	for st in states:
+		st.mount(dataTrans, states)
+		st.print()
