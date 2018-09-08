e2c8302b8436a84ba4a6872d79b495688d4b2b74
everton-zoio <evertonabreu@alunos.utfpr.edu.br>
2018-09-08 20:50:23 -0300

    Pronto para a execução de Maquinas de Turing deterministicas

--- automat.py
+++ automat.py
@@ -1,5 +1,7 @@
 import sys
 
+MaxExec = 500
+
 class Transition():
 	state = 0
 	newState = 0
@@ -26,18 +28,35 @@ class Transition():
 		return 0
 		pass
 
+	def direct(self):
+		return self.direction
+		pass
+
+	def nextValue(self):
+		return self.newValue
+		pass
+
+	def exe(self, value):
+		if(self.valid(value)):
+			return self.newState
+			pass
+		else:
+			return 0
+		pass
+
 	def print(self):
 		print("\t",self.state.cod, self.newState.cod, self.value, self.newValue, self.direction)
 		pass
 	pass
 
 class State():
+
 	def __init__(self, cod, fStates):
 		self.cod = cod
-		self.type = 0
+		self.final = False
 		for fS in fStates:
 			if(fS == self.cod):
-				self.type = 1
+				self.final = True
 				break
 		self.transitions = []
 		pass
@@ -47,38 +66,145 @@ class State():
 			if(int(tr.split()[0]) == self.cod):
 				self.transitions.append(Transition(tr, self, states))
 		pass
+	
+	def exe(self, value):
+		for tr in self.transitions:
+			if(tr.valid(value)):
+				return tr.exe(value)
+		return 0
+		pass
+
+	def selectDirection(self, pos):
+		if(pos >= 0 & pos < len(self.transitions)):
+			b = self.transitions[pos].direct()
+			return b
+		pass
+
+	def selectValue(self, pos):
+		if(pos >= 0 & pos < len(self.transitions)):
+			return self.transitions[pos].nextValue()
+		pass
+
+	def trans(self, value):
+		vet = []
+		count = 0
+		for tr in self.transitions:
+			if (tr.valid(value)):
+				vet.append(count)
+			count += 1
+		return vet
+		pass
+
+	def exeSpec(self, pos, value):
+		if(pos >= 0 & pos < len(self.transitions)):
+			return self.transitions[pos].exe(value)
+		pass
+
+	def isFinal(self):
+		return self.final
+		pass
+	
 	def print(self):
 		print("  ", self.cod)
 		for tr in self.transitions:
 			tr.print()
-		print("\n")
-		pass
-
-class Machine():
-
-	def __init__(self, IState, string):
-		self.state = IState
-		self.tape = string
+		print("\r")
 		pass
 	pass
 
-	def main(self):
-		self.head = 0
-		for tr in self.state.transitions
-			if(self.tape[head] == tr.value)
-				self.tape[head] = tr.newValue
-				self.state = tr.newState
-				if(tr.direction == 'R')
-					self.head++
-				if(tr.direction == 'L')
-					self.head--
+class Tape():
 
+	def __init__(self, tape, brank):
+		self.head = 3
+		self.tape = ''
+		self.brank = brank
+		if(isinstance(tape, Tape)):
+			self.tape = tape.content()
+			self.head = tape.headR()
+			pass
+		else:
+			self.tape = [brank, brank, brank]
+			self.tape.extend(list(tape))
+			self.tape.extend([brank, brank, brank])
+		pass
+	
+	def headR(self):
+		value = self.head
+		return value
+		pass
+	
+	def content(self):
+		tp = self.tape
+		return tp
+		pass
+
+	def value(self):
+		return self.tape[self.head]
+		pass
+
+	def reflesh(self, newValue, direction):
+		self.tape[self.head] = newValue
+		if((self.head + 1) == len(list(self.tape))):
+			self.tape.extend([self.brank, self.brank, self.brank])
+			pass
+		elif (self.head == 0):
+			aux = [self.brank, self.brank, self.brank]
+			aux.extend(self.tape)
+			self.tape = aux
+			self.head += 3
+			pass
+		if(direction == 'R'):
+			self.head += 1
+			pass
+		elif(direction == 'L'):
+			self.head -= 1
+			pass
+		pass
+
+	def print(self):
+		print("> > > > "+"".join(self.tape)+" < < < <")
+		pass
+	pass
+
+class Machine():
+
+	def __init__(self, IState, tape, brank):
+		self.state = IState
+		self.oldState = None
+		self.staked = 0
+		self.tape = Tape(tape, brank)
+		pass
+
+	def main(self):
+		self.tape.print()
+		v = self.tape.value()
+		s = self.state.trans(v)
+		if(len(s) > 0):
+			if(self.oldState == self.state):
+				self.staked += 1
+			else:
+				self.staked = 0
+			self.oldState = self.state
+			d = self.state.selectDirection(s[0])
+			nV = self.state.selectValue(s[0])
+			self.state = self.state.exeSpec(s[0], v)
+			self.tape.reflesh(nV,d)
+			if(self.state.isFinal()):
+				return 2
+			if(self.staked > MaxExec):
+				return -1
+			return 1
+		else:
+			return 0
+		pass
+	pass
 
 if(sys.argv.__len__() >= 2):
-	arq = open(sys.argv[1],)
+	arq = open(sys.argv[1], 'r')
 	data = arq.readlines()
 	dataStates = list(map(int, data[3].split()))
 	dataTrans = data[7:]
+	brank = data[2].split()[0]
 	fStates = data[5].split()
 	fStates = list(map(int, fStates))
 	states = []
@@ -86,9 +212,20 @@ if(sys.argv.__len__() >= 2):
 		states.append(State(st,fStates))
 	for st in states:
 		st.mount(dataTrans, states)
-		st.print()
 	IState = int(data[4].split()[0])
 	for st in states:
 		if(st.cod == IState):
 			IState = st
-	maq = Machine(IState, sys.argv[2])
+			break
+	
+	maq = Machine(IState, sys.argv[2], brank)
+	a = 1
+	while a == 1:
+		a = maq.main()
+		pass
+	if (a == -1):
+		print("A maquina entrou em loop!!!")
+	elif (a == 0):
+		print("A palavra não era valida!!!")
+	else:
+		print("A palavra era valida!!!")
